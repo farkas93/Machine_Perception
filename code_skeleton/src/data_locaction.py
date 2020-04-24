6ce1dff6ad790b68ea6362917fbb07bca2df4e51
farkas93 <zsombor.kalotay@gmx.ch>
2020-04-24 16:30:32 +0200

    remove debug stuff and change data location to leonhard path

--- code_skeleton/src/data_locaction.py
+++ code_skeleton/src/data_locaction.py
@@ -5,7 +5,7 @@ dataconfig['localfolder'] = 'K:/MLData/MP2020_data'
 dataconfig['leonhard'] = '/cluster/project/infk/hilliges/lectures/mp20/project3'
 
 # Set the data location for training
-dataconfig['location'] = dataconfig['localfolder']
+dataconfig['location'] = dataconfig['leonhard']
 
 # Set datalocation for training, test and validation dataset
 dataconfig['train_data'] = dataconfig['location'] + 'mp20_train.h5'
