d9af77580025229bfa2dab1d80a8c1e89275dfcd
farkas93 <zsombor.kalotay@gmx.ch>
2020-04-24 16:34:09 +0200

    jan localfolder

--- code_skeleton/src/data_locaction.py
+++ code_skeleton/src/data_locaction.py
@@ -1,7 +1,8 @@
 dataconfig = dict()
 
 # Store your possible data locations here
-dataconfig['localfolder'] = 'K:/MLData/MP2020_data'
+dataconfig['zsombor_localfolder'] = 'K:/MLData/MP2020_data'
+dataconfig['jan_localfolder'] = ''
 dataconfig['leonhard'] = '/cluster/project/infk/hilliges/lectures/mp20/project3'
 
 # Set the data location for training
