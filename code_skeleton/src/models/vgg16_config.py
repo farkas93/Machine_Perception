8bd540f97f6ea433bd9cf40b0c6bd963d87ba971
farkas93 <zsombor.kalotay@gmx.ch>
2020-04-24 11:07:01 +0200

    fix config?

--- code_skeleton/src/models/vgg16_config.py
+++ code_skeleton/src/models/vgg16_config.py
@@ -24,10 +24,9 @@ config['metrics'] = 'mse'
 config['model'] ='vggnet'
 
 # General model configs
-config['vggnet'] = {}
-config['vggnet']['num_filters'] = [64, 128, 256, 512, 512]    # Number of filters for every convolutional layer.
-config['vggnet']['filter_size'] = [3, 3, 3, 3, 3]             # Kernel size for convoluional layers.
-config['vggnet']['strides'] = [1, 1, 2, 2, 2]                 # Strides for MaxPool layers
-config['vggnet']['pool_size'] = [2, 2, 2, 2, 2]               # Pool_size for MaxPool layers
-config['vggnet']['learning_rate'] = 1e-4
-config['vggnet']['metrics'] = config['metrics']
+config
+config['num_filters'] = [64, 128, 256, 512, 512]    # Number of filters for every convolutional layer.
+config['filter_size'] = [3, 3, 3, 3, 3]             # Kernel size for convoluional layers.
+config['strides'] = [1, 1, 2, 2, 2]                 # Strides for MaxPool layers
+config['pool_size'] = [2, 2, 2, 2, 2]               # Pool_size for MaxPool layers
+config['learning_rate'] = 1e-4
