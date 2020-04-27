cbc3d51911ea2ee0e7873b6978944af45f1ce681
farkas93 <zsombor.kalotay@gmx.ch>
2020-04-27 08:54:42 +0200

    another parameter for leonhard

--- code_skeleton/src/models/vgg16_config.py
+++ code_skeleton/src/models/vgg16_config.py
@@ -8,14 +8,14 @@ vgg_config = dict()
 vgg_config['n_epochs'] = 15
 vgg_config['batch_size'] = 16
 vgg_config['learning_rate'] = 1e-4
+vgg_config['reduce_lr_after_steps'] = 15000
+vgg_config['lr_multiplier_gain'] = 0.5
 vgg_config['eye_patch'] = 'left-eye' #'right-eye', 'eye-region'
+vgg_config['loss_terms'] = ['gaze_mse']
+vgg_config['metrics'] = ['gaze_angular']
 
 # vgg_config['training_steps_per_epoch'] = 1
 # vgg_config['validation_steps_per_epoch'] = 1
-vgg_config['loss_terms'] = ['gaze_mse']
-vgg_config['metrics'] = ['gaze_angular']
-vgg_config['reduce_lr_after_steps'] = 5000
-vgg_config['lr_multiplier_gain'] = 0.5
 
 # General model configs
 vgg_config['num_filters'] = [64, 128, 256, 512, 512]    # Number of filters for every convolutional layer.
