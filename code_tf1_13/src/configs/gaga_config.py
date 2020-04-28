a1bcc35cf00bb3082b1667a383f6992139fbf267
farkas93 <zsombor.kalotay@gmx.ch>
2020-04-28 10:37:28 +0200

    Next run on leonhard (Mk8)

--- code_tf1_13/src/configs/gaga_config.py
+++ code_tf1_13/src/configs/gaga_config.py
@@ -1,11 +1,11 @@
 gaga_config = dict()
 
 # General configs for training
-gaga_config['n_epochs'] = 15
+gaga_config['n_epochs'] = 20
 gaga_config['batch_size'] = 16
-gaga_config['learning_rate'] = 1e-4
-gaga_config['reduce_lr_after_steps'] = 5000
-gaga_config['lr_multiplier_gain'] = 0.8
+gaga_config['learning_rate'] = 1e-5
+gaga_config['reduce_lr_after_steps'] = 40000
+gaga_config['lr_multiplier_gain'] = 0.1
 gaga_config['loss_terms'] = ['gaze_mse']
 gaga_config['metrics'] = ['gaze_angular']
 
@@ -14,3 +14,4 @@ gaga_config['num_filters'] = [64, 128, 256, 512, 512]    # Number of filters for
 gaga_config['filter_size'] = [3, 3, 3, 3, 3]             # Kernel size for convoluional layers.
 gaga_config['strides'] = [1, 2, 2, 2, 2]                 # Strides for MaxPool layers
 gaga_config['pool_size'] = [2, 2, 2, 2, 2]               # Pool_size for MaxPool layers
+gaga_config['dropout_seed'] = 2251
